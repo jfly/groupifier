324087aaa60d3df85c0ff334b5f3f17dc4c3b161
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-08-11 01:52:42 +0200

    Add an ability to specify stations and scramblers count.

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ import { parse as parseCSV } from 'papaparse';
 import { allEvents } from './events';
 
 const fileInput = document.getElementById('file-input');
+const stationsInput = document.getElementById('stations-input');
+const scramblersInput = document.getElementById('scramblers-input');
 const button = document.getElementById('generate');
 
 button.addEventListener('click', () => {
+  const stationsCount = parseInt(stationsInput.value);
+  const scramblersCount = parseInt(scramblersInput.value);
   parseCSV(fileInput.files[0], {
     header: true,
     complete: ({ data: rows }) => {
       const people = rows.map(row => {
         const person = { name: row['Name'], wcaId: row['WCA ID'], events: [] };
         allEvents.forEach(event => {
-          if(row[event.id] === "1") {
+          if(row[event.id] === '1') {
             person.events.push(event.id);
           }
         });
