5e323950760ef5aa429233418d8492524924c270
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-08-15 00:58:34 +0200

    Format names so that they look good and are sorted properly.

--- src/main.js
+++ src/main.js
@@ -53,8 +53,9 @@ button.addEventListener('click', () => {
 });
 
 function peopleFromCsvRows(rows) {
-  return _.sortBy(rows, 'Name').map(row => {
-    const person = { name: row['Name'], wcaId: row['WCA ID'], events: [], solving: {}, scrambling: {}, judging: {} };
+  const people = rows.map(row => {
+    const name = row['Name'].split(/\s+/).map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+    const person = { name, wcaId: row['WCA ID'], events: [], solving: {}, scrambling: {}, judging: {} };
     eventObjects.forEach(eventObject => {
       if(row[eventObject.id] === '1') {
         person.events.push(eventObject.id);
@@ -62,4 +63,5 @@ function peopleFromCsvRows(rows) {
     });
     return person;
   });
+  return _.sortBy(people, 'name');
 }
