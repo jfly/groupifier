daf51b4f38146d45876f8fda43a8dc2ee4966c6b
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-11-30 19:17:37 +0100

    Display simultaneous events.

--- src/main.js
+++ src/main.js
@@ -75,15 +75,30 @@ function peopleFromCsvRows(rows) {
   return _.sortBy(people, 'name');
 }
 
-const eventListElements = eventObjects
-  .map(eventObject => `<option value="${eventObject.id}">${eventObject.name}</option>`)
-  .join('\n');
-document.querySelectorAll('.event-selectfield select').forEach(element => element.innerHTML = eventListElements);
+const eventOptions = eventObjects.map(eventObject => `<option value="${eventObject.id}">${eventObject.name}</option>`);
+document.querySelectorAll('.event-selectfield select').forEach(element => {
+  element.innerHTML = ['<option value="" selected></option>', ...eventOptions].join('\n');
+});
 
 const addSimultaneousEvent = document.getElementById('add-simultaneous-event');
 const mainEvent = document.getElementById('main-event');
 const sideEvent = document.getElementById('side-event');
+const simultaneousEventsFormRow = document.getElementById('simultaneous-events-form');
 
+const selectedText = element => element.options[element.selectedIndex].innerHTML;
 addSimultaneousEvent.addEventListener('click', () => {
-  console.log(mainEvent.value, sideEvent.value);
+  if (mainEvent.value && sideEvent.value) {
+    simultaneousEventsFormRow.insertAdjacentHTML('beforebegin', `
+      <tr class="simultaneous-events-pair" data-main-event="${mainEvent.value}" data-side-event="${sideEvent.value}">
+        <td class="mdl-data-table__cell--non-numeric">${selectedText(mainEvent)}</td>
+        <td class="mdl-data-table__cell--non-numeric">${selectedText(sideEvent)}</td>
+        <td class="mdl-data-table__cell--non-numeric">
+          <button type="button" class="mdl-button mdl-js-button mdl-button--icon" onclick="this.parentNode.parentNode.remove()">
+            <i class="material-icons">delete</i>
+          </button>
+        </td>
+      </tr>
+    `);
+    mainEvent.value = sideEvent.value = '';
+  }
 });
