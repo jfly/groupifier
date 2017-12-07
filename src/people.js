a464a017d959d422f6eb9a5d78422a152c827dd3
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-12-07 18:11:59 +0100

    Add ID for each person, just like Cubecomps does.

--- src/people.js
+++ src/people.js
@@ -3,9 +3,9 @@ import _ from 'lodash';
 import { eventObjects } from './events';
 
 export function peopleFromCsvRows(rows) {
-  const people = rows.map(row => {
+  const people = rows.map((row, index) => {
     const name = row['Name'].split(/\s+/).map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
-    const person = { name, wcaId: row['WCA ID'].toUpperCase(), events: [], solving: {}, scrambling: {}, judging: {} };
+    const person = { id: index + 1, name, wcaId: row['WCA ID'].toUpperCase(), events: [], solving: {}, scrambling: {}, judging: {} };
     person.events = _.map(eventObjects, 'id').filter(eventId => row[eventId] === '1');
     return person;
   });
