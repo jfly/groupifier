721ff13bf4bd5e3f7a9efeaeb57fedbace90163b
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-12-26 15:42:06 +0100

    Clean up summary pdf.

--- src/pdfs/summary-pdf.js
+++ src/pdfs/summary-pdf.js
@@ -6,34 +6,31 @@ import { eventObjects } from '../events';
 export class SummaryPdf extends PdfDocument {
   constructor(eventsWithData) {
     super('summary.pdf');
-    const groupsByEvent = _.mapValues(_.fromPairs(eventsWithData), 'groups');
-    this.definition = {
-      content: eventObjects
-        .filter(eventObject => groupsByEvent[eventObject.id])
-        .map(eventObject =>
-          groupsByEvent[eventObject.id].map(group =>
-            [
-              {
-                unbreakable: true,
-                margin: [0, 0, 0, 10],
-                stack: [
-                  { text: `${eventObject.name} - Group ${group.id}`, bold: true, fontSize: 14, margin: [0, 0, 0, 5] },
-                  {
-                    fontSize: 8,
-                    columns: ['Solving', 'Scrambling', 'Judging'].map(type => {
-                      const people = group[`people${type}`];
-                      if(!people) return {};
-                      return [
-                        { text: `${type} (${people.length})`, bold: true, fontSize: 10, margin: [0, 0, 0, 2] },
-                        { ul: _.map(people, 'name').sort() }
-                      ];
-                    })
-                  }
-                ]
-              }
-            ]
-          )
-        )
+    const groupsSummary = _.flatMap(eventsWithData, ([eventId, { groups }]) =>
+      _.flatMap(groups, group => this.groupSummary(eventId, group))
+    );
+    this.definition = { content: groupsSummary };
+  }
+
+  groupSummary(eventId, group) {
+    const eventObject = _.find(eventObjects, { id: eventId });
+    return {
+      unbreakable: true,
+      margin: [0, 0, 0, 10],
+      stack: [
+        { text: `${eventObject.name} - Group ${group.id}`, bold: true, fontSize: 14, margin: [0, 0, 0, 5] },
+        {
+          fontSize: 8,
+          columns: ['Solving', 'Scrambling', 'Judging'].map(type => {
+            const people = group[`people${type}`];
+            if(!people) return {};
+            return [
+              { text: `${type} (${people.length})`, bold: true, fontSize: 10, margin: [0, 0, 0, 2] },
+              { ul: _.map(people, 'name').sort() }
+            ];
+          })
+        }
+      ]
     };
   }
 }
