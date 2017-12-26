1115b9f5aeda29f9ec476d9479f814e822c8ffe7
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-12-26 01:08:26 +0100

    Clean up the main logic.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,11 @@ import 'mdl-selectfield/dist/mdl-selectfield.js';
 import '../assets/main.css';
 import 'spinkit/css/spinners/11-folding-cube.css';
 
-import { parse as parseCSV } from 'papaparse';
 import _ from 'lodash';
 
 import { $, $all } from './helpers';
 import { signIn, signOut, isSignedIn, getUpcomingManageableCompetitions, getCompetitionWcif } from './wca-api';
-import { peopleFromCsvRows, attachWcaDataToPeople } from './people';
+import { peopleFromCsvFile, peopleWithWcaData } from './people';
 import { assignGroups, assignScrambling, assignJudging } from './groups-assignment';
 import { createPersonalCardsPdf, createSummaryPdf, createScorecardsPdf } from './pdf-creation';
 import { sideEventsByMainEvents } from './simultaneous-events';
@@ -42,31 +41,29 @@ const loadingScreen = shown => document.body.classList.toggle('loading', shown);
 $('#generate').addEventListener('click', () => {
   loadingScreen(true);
   const competitionId = $('#competition-id-select').value;
+  const registrationsFile = $('#file-input').files[0];
   const stationsCount = parseInt($('#stations-input').value);
   const scramblersCount = parseInt($('#scramblers-input').value);
   const staffJudgesCount = parseInt($('#staff-judges-input').value);
   const sortByResults = $('#sort-by-results-input').checked;
   const askForScramblers = $('#ask-for-scramblers-input').checked;
   const skipNewcomers = $('#skip-newcomers-input').checked;
-  parseCSV($('#file-input').files[0], {
-    header: true,
-    skipEmptyLines: true,
-    complete: ({ data: rows }) => {
-      const people = peopleFromCsvRows(rows);
-      Promise.all([getCompetitionWcif($('#competition-id-select').value), attachWcaDataToPeople(people)]).then(([wcif]) => {
-        const eventsWithData = assignGroups(people, stationsCount, sortByResults, sideEventsByMainEvents());
-        return assignScrambling(eventsWithData, scramblersCount, askForScramblers, skipNewcomers).then(() => {
-          assignJudging(people, eventsWithData, stationsCount, staffJudgesCount, skipNewcomers);
-          return Promise.all([
-            new Promise(resolve => createPersonalCardsPdf(people).download('personal-cards.pdf', resolve)),
-            new Promise(resolve => createSummaryPdf(eventsWithData).download('summary.pdf', resolve)),
-            new Promise(resolve => createScorecardsPdf(eventsWithData, wcif).download('scorecards.pdf', resolve))
-          ]);
-        });
-      })
-      .then(() => loadingScreen(false), () => loadingScreen(false));
-    }
-  });
+  Promise.all([
+    peopleFromCsvFile(registrationsFile).then(peopleWithWcaData),
+    getCompetitionWcif(competitionId)
+  ])
+  .then(([people, wcif]) => {
+    const eventsWithData = assignGroups(people, stationsCount, sortByResults, sideEventsByMainEvents());
+    return assignScrambling(eventsWithData, scramblersCount, askForScramblers, skipNewcomers).then(() => {
+      assignJudging(people, eventsWithData, stationsCount, staffJudgesCount, skipNewcomers);
+      return Promise.all([
+        new Promise(resolve => createPersonalCardsPdf(people).download('personal-cards.pdf', resolve)),
+        new Promise(resolve => createSummaryPdf(eventsWithData).download('summary.pdf', resolve)),
+        new Promise(resolve => createScorecardsPdf(eventsWithData, wcif).download('scorecards.pdf', resolve))
+      ]);
+    });
+  })
+  .then(() => loadingScreen(false), () => loadingScreen(false));
 });
 
 /* Form initialization */
--- src/people.js
+++ src/people.js
@@ -1,22 +1,35 @@
+import { parse as parseCSV } from 'papaparse';
 import _ from 'lodash';
 
 import { eventObjects } from './events';
 import { getPeopleData } from './wca-api';
 
-export function peopleFromCsvRows(rows) {
-  const people = rows.map((row, index) => {
-    const name = row['Name'].split(/\s+/).map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
-    const person = { id: index + 1, name, wcaId: row['WCA ID'].toUpperCase(), events: [], solving: {}, scrambling: {}, judging: {} };
-    person.events = _.map(eventObjects, 'id').filter(eventId => row[eventId] === '1');
-    return person;
-  });
-  return _.sortBy(people, 'name');
-}
-
-export function attachWcaDataToPeople(people) {
-  return getPeopleData(_.compact(_.map(people, 'wcaId'))).then(peopleData => {
-    peopleData.forEach(personData => {
-      _.find(people, { wcaId: personData.person.wca_id }).wcaData = personData;
+export function peopleFromCsvFile(file) {
+  return new Promise(resolve => {
+    parseCSV(file, {
+      header: true,
+      skipEmptyLines: true,
+      complete: ({ data: rows }) => resolve(
+        rows.map((row, index) => ({
+          id: index + 1,
+          name: row['Name'].split(/\s+/).map(_.capitalize).join(' '),
+          wcaId: row['WCA ID'].toUpperCase(),
+          events: _.map(eventObjects, 'id').filter(eventId => row[eventId] === '1'),
+          solving: {},
+          scrambling: {},
+          judging: {}
+        }))
+      )
     });
   });
+}
+
+export function peopleWithWcaData(people) {
+  return getPeopleData(_.compact(_.map(people, 'wcaId')))
+    .then(peopleData => {
+      peopleData.forEach(personData => {
+        _.find(people, { wcaId: personData.person.wca_id }).wcaData = personData;
+      });
+    })
+    .then(() => people);
 }
