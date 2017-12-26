477641e64cd9ff4dc587b36a259cabbe266be349
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-12-26 15:32:18 +0100

    Clean up personal cards pdf.

--- src/pdfs/personal-cards-pdf.js
+++ src/pdfs/personal-cards-pdf.js
@@ -7,33 +7,33 @@ export class PersonalCardsPdf extends PdfDocument {
   constructor(people) {
     super('personal-cards.pdf');
     const competitionEvents = _(people).map('events').flatten().uniq().value();
-    const personalCards = people.map(person => {
-      const tableBody = eventObjects
-        .filter(eventObject => competitionEvents.includes(eventObject.id))
-        .map(eventObject => {
-          const groupsText = task => ({ text: (person[task][eventObject.id] || []).join(', '), alignment: 'center' });
-          return [eventObject.name, groupsText('solving'), groupsText('scrambling'), groupsText('judging')];
-        });
-      return [
-        { text: `${person.name}`, bold: true },
-        { text: person.wcaId ? `WCA ID: ${person.wcaId}` : " " },
-        {
-          table: {
-            body: [['Event', 'Solving', 'Scrambling', 'Judging'], ...tableBody]
-          },
-          layout: { paddingLeft: () => 2, paddingRight: () => 2, paddingTop: () => 1, paddingBottom: () => 1 }
-        }
-      ];
-    });
-    /* Force the presence of three cards for each row. */
-    while(personalCards.length % 3) {
-      personalCards.push([]);
-    }
+    const personalCards = people.map(person => this.personalCard(competitionEvents, person));
+    const personalCardsInLastRow = personalCards.length % 3;
+    personalCardsInLastRow === 0 || personalCards.push(..._.times(3 - personalCardsInLastRow, _.constant({})));
     this.definition = {
+      pageMargins: [5, 5, 5, 5],
       content: _.chunk(personalCards, 3).map(cards => (
         { columns: cards, fontSize: 8, margin: [0, 0, 0, 10] , unbreakable: true }
-      )),
-      pageMargins: [5, 5, 5, 5]
+      ))
     };
+  }
+
+  personalCard(competitionEvents, person) {
+    const tableBody = eventObjects
+      .filter(eventObject => competitionEvents.includes(eventObject.id))
+      .map(eventObject => {
+        const groupsText = task => ({ text: (person[task][eventObject.id] || []).join(', '), alignment: 'center' });
+        return [eventObject.name, groupsText('solving'), groupsText('scrambling'), groupsText('judging')];
+      });
+    return [
+      { text: `${person.name}`, bold: true },
+      { text: person.wcaId ? `WCA ID: ${person.wcaId}` : " " },
+      {
+        table: {
+          body: [['Event', 'Solving', 'Scrambling', 'Judging'], ...tableBody]
+        },
+        layout: { paddingLeft: () => 2, paddingRight: () => 2, paddingTop: () => 1, paddingBottom: () => 1 }
+      }
+    ];
   }
 }
